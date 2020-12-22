834298d012c3921e694e1a23c7dbcc63edd4f1ff
Bartek Szczeciński <bartosz.szczecinski@protonmail.com>
2020-12-22 15:08:57 +0100

    Add left side panel with decks and counter

--- client/src/pages/game/index.jsx
+++ client/src/pages/game/index.jsx
@@ -3,11 +3,18 @@ import './style.css';
 import React from 'react';
 
 import { CardsInHand } from './CardsInHand';
+import { Counter } from './Counter';
+import { Deck } from './Deck';
 import { cardsInHandData } from './dummyData';
 
 const Game = () => (
   <div className="game">
     <CardsInHand cards={cardsInHandData}></CardsInHand>
+    <div className="left-side-panel">
+      <Deck color="black" cardsLeft={23} cardsMax={40}></Deck>
+      <Counter></Counter>
+      <Deck color="white" cardsLeft={44} cardsMax={80}></Deck>
+    </div>
   </div>
 );
 
