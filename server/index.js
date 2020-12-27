eca8016ed2f993c54afb2d29d97f2fe7affa4468
jakub-galecki <324384@uwr.edu.pl>
2020-12-27 17:13:36 +0100

    Add websockets to server.

--- server/index.js
+++ server/index.js
@@ -1,11 +1,20 @@
+'use strict';
+
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const WebSocket = require('ws');
 const apiRouter = require('./routes/api');
-
+const http = require('http');
 const uri = 'mongodb://127.0.0.1:27017/CAH';
-mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false}).catch((err) => console.error(err.reason));
+
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+}).catch((err) => console.error(err.reason));
 
 const app = express();
 app.use(cors());
@@ -15,7 +24,34 @@ app.use(bodyParser.urlencoded({extended: true}));
 require('./models/UserSchema');
 app.use('/api/', apiRouter);
 
+const server = http.createServer(app);
+const wss = new WebSocket.Server({server, path: '/game'});
 
-const server = app.listen(process.env.PORT || 8080, ()=>{
+server.listen(process.env.PORT || '8080', () => {
     console.log('Listening on port: ' + server.address().port);
 });
+
+
+wss.on('connection', (ws, request) => {
+    ws.isAlive = true;
+    ws.on('pong', () => {
+        ws.isAlive = true;
+    });
+    ws.on('message', (message) => {
+        console.log(message);
+    });
+});
+
+const interval = setInterval(() => {
+    wss.clients.forEach((ws) => {
+        if (!ws.isAlive) return ws.terminate();
+        ws.isAlive = false;
+        ws.ping(() => {
+        });
+    });
+}, 5000);
+
+wss.on('close', () => {
+    clearInterval(interval);
+});
+
