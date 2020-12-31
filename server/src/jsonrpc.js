58797fde0c15d79b0261c32d36806a994ea10571
jakub-galecki <324384@uwr.edu.pl>
2020-12-31 15:05:17 +0100

    Add Handle request.

---
+++ server/src/jsonrpc.js
@@ -0,0 +1,33 @@
+// https://www.youtube.com/watch?v=6XzGmdXbZmU&ab_channel=GolecuOrkiestra
+const Request = require('./request');
+const {ParseError, InvalidRequest} = require('./err');
+/**
+ *
+ * @param {Object} obj
+ * @return {Request}
+ */
+function createRpcObj(obj) {
+    if (Request.isRequest(obj)) {
+        Request.parseRequest(obj);
+        const {id, jsonrpc, method, params} = obj;
+        return new Request(jsonrpc, id, method, params);
+    }
+    throw new InvalidRequest('Invalid request was sent');
+}
+/**
+ * @param {string} message
+ * @return {Object}
+ */
+module.exports.parse = function(message) {
+    let obj;
+    try {
+        obj = JSON.parse(message);
+    } catch (e) {
+        throw new ParseError(e.message);
+    }
+    try {
+        return createRpcObj(obj);
+    } catch (e) {
+        throw new InvalidRequest(e.message);
+    }
+};
