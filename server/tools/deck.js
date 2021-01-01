7bd6bab149502449ebfe6994aa7954c045f7eff2
MBronko <michal.brono@protonmail.com>
2021-01-01 18:57:24 +0100

    Fix remove redundand variables

--- server/tools/deck.js
+++ server/tools/deck.js
@@ -3,19 +3,14 @@ const {InternalError} = require('../src/err');
 require('../models/DeckSchema');
 const Deck = mongoose.model('Deck');
 
-module.exports.getData = async function(func, params) {
-    return await func(params);
-};
-
 // {"jsonrpc": "2.0", "method": "deck.createDeck", "params": {"title":"tytul", "userId":"1"}, "id": 1}
-// {"jsonrpc": "2.0", "method": "deck.getDeck", "params": {"id":"67QY5Z"}, "id": 1}
+// {"jsonrpc": "2.0", "method": "deck.getDeck", "params": {"id":"07ZZ4G"}, "id": 1}
 // {"jsonrpc": "2.0", "method": "deck.getAllDecks", "params": {}, "id": 1}
-// {"jsonrpc": "2.0", "method": "deck.updateDeck", "params": {"id":"67QY5Z", "title":"nowy tytul"}, "id": 1}
-// {"jsonrpc": "2.0", "method": "deck.deleteDeck", "params": {"id":"67QY5Z"}, "id": 1}
+// {"jsonrpc": "2.0", "method": "deck.updateDeck", "params": {"id":"07ZZ4G", "title":"nowy tytul"}, "id": 1}
+// {"jsonrpc": "2.0", "method": "deck.deleteDeck", "params": {"id":"07ZZ4G"}, "id": 1}
 
 module.exports.getDeck = async function getDeck(params) {
-    const deck = Deck.findOne({'shortId': params.id}).exec();
-    return await deck.then(function(data) {
+    return await Deck.findOne({'shortId': params.id}).exec().then(function(data) {
         return data;
     }).catch(function(error) {
         throw new InternalError('Deck not found');
@@ -59,8 +54,7 @@ module.exports.updateDeck = async function updateDeck(params) {
         throw new InternalError('Deck title is empty');
     }
 
-    const deck = Deck.findOneAndUpdate({shortId: params.id}, {title: params.title}, {new: true}).exec();
-    return await deck.then(function(deck) {
+    return await Deck.findOneAndUpdate({shortId: params.id}, {title: params.title}, {new: true}).exec().then(function(deck) {
         return deck;
     }).catch(function(error) {
         throw new InternalError(error.message);
@@ -69,8 +63,7 @@ module.exports.updateDeck = async function updateDeck(params) {
 
 module.exports.deleteDeck = async function deleteDeck(params) {
     if (!params.id) throw new InternalError('You must provide deck id');
-    const deck = Deck.findOneAndRemove({shortId: params.id}).exec();
-    return await deck.then(function(deck) {
+    return await Deck.findOneAndRemove({shortId: params.id}).exec().then(function(deck) {
         return 'deleted';
     }).catch(function(error) {
         throw new InternalError('Deck not found');
