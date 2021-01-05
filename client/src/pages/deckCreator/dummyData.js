ddba770215f21307df58f9d914b0596a35c43f5e
Bartek Szczeciński <bartosz.szczecinski@protonmail.com>
2021-01-06 00:39:07 +0100

    Add deck state logic

---
+++ client/src/pages/deckCreator/dummyData.js
@@ -0,0 +1,28 @@
+const myDeck1Data = {
+  id: 'ABCDE',
+  type: 'answers',
+  cards: [
+    { cardText: 'Card 1', id: 1 },
+    { cardText: 'Another card', id: 2 },
+    { cardText: 'An answer', id: 3 },
+    { cardText: 'This is an answer', id: 4 },
+    { cardText: 'Card 5', id: 5 },
+    { cardText: 'This is an answer', id: 6 },
+    { cardText: 'This is an answer', id: 7 },
+    { cardText: 'This is an answer', id: 8 },
+    { cardText: 'This is an answer', id: 9 },
+    { cardText: 'This is an answer', id: 10 },
+    { cardText: 'Card 11', id: 11 },
+    { cardText: 'Another card', id: 12 },
+    { cardText: 'An answer', id: 13 },
+    { cardText: 'This is an answer', id: 14 },
+    { cardText: 'Card 15', id: 15 },
+    { cardText: 'This is an answer', id: 16 },
+    { cardText: 'This is an answer', id: 17 },
+    { cardText: 'This is an answer', id: 18 },
+    { cardText: 'This is an answer', id: 19 },
+    { cardText: 'This is an answer', id: 20 },
+  ],
+};
+
+export { myDeck1Data };
--- client/src/pages/deckCreator/index.jsx
+++ client/src/pages/deckCreator/index.jsx
@@ -1,13 +1,53 @@
 import './style.scss';
 
-import React from 'react';
+import React, { useState } from 'react';
 
-import { EditableCard } from '../../components/deckCreator/cards/EditableCard';
+import { DeckCards } from '../../components/deckCreator/cards/DeckCards';
+import { myDeck1Data } from './dummyData';
 
-const DeckCreator = () => (
-  <div className="deck-creator">
-    <EditableCard>This is card content</EditableCard>
-  </div>
-);
+const DeckCreator = () => {
+  const [myDeck, setMyDeck] = useState(myDeck1Data);
+
+  const updateCardInDeck = (cardID, newContent) => {
+    console.log(
+      `Updating card ${cardID} to "${newContent}". Send this to server`,
+    );
+    const newCards = myDeck.cards.map((card) =>
+      cardID === card.id ? { cardText: newContent, id: cardID } : card,
+    );
+    setMyDeck({ ...myDeck, cards: newCards });
+  };
+
+  const addCardToDeck = (newContent) => {
+    console.log(`Adding new card "${newContent}" to deck. Send this to server`);
+
+    const newCards = Array.from(myDeck.cards);
+    //TODO: Change id from random to a value from server
+    newCards.unshift({
+      cardText: newContent,
+      id: Math.floor(Math.random() * 5000) + 100,
+    });
+
+    setMyDeck({ ...myDeck, cards: newCards });
+  };
+
+  const removeCardFromDeck = (cardID) => {
+    console.log(`Removing card ${cardID} from deck. Send this to server`);
+
+    const newCards = myDeck.cards.filter((card) => card.id !== cardID);
+    setMyDeck({ ...myDeck, cards: newCards });
+  };
+
+  return (
+    <div className="deck-creator">
+      <DeckCards
+        deck={myDeck}
+        updateCard={updateCardInDeck}
+        addCard={addCardToDeck}
+        removeCard={removeCardFromDeck}
+      ></DeckCards>
+    </div>
+  );
+};
 
 export { DeckCreator };
