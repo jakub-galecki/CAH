8a565a9b7db9608ce57ec70853ed5be07a514743
Bartek Szczeciński <bartosz.szczecinski@protonmail.com>
2021-01-20 20:24:56 +0100

    Add load decks from server in lobby

--- client/src/pages/lobby/index.jsx
+++ client/src/pages/lobby/index.jsx
@@ -1,21 +1,23 @@
 import './style.scss';
 
-import React, { useEffect,useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
-import { generateDecks } from '../../components/lobby/cardPanel/fakeDecks';
+import { addGeneratedDeckToServer } from '../../components/lobby/cardPanel/fakeDecks';
 import { CardPanel } from '../../components/lobby/cardPanel/index';
 import { Overview } from '../../components/lobby/overview/index';
 import { Leaderboard } from '../../components/shared/leaderboard/Leaderboard';
+import { useAuth } from '../../contexts/auth';
 import { useConnection } from '../../contexts/connection';
 import { useRoom } from '../../contexts/room';
 
 const Lobby = () => {
-  const [availableDecks, _setAvailableDecks] = useState(generateDecks(18));
+  const [availableDecks, setAvailableDecks] = useState([]);
   const [chosenDecks, setChosenDecks] = useState([]);
   const [leaderBoardData, setLeaderBoardData] = useState([]);
-  const [isAdmin, setIsAdmin] = useState(true);
+  const [isAdmin, setIsAdmin] = useState(false);
 
   const { rpc } = useConnection();
+  const { userId } = useAuth();
   const { roomId } = useRoom();
   const isDeckChosen = (deckID) => chosenDecks.some((d) => d.id === deckID);
 
@@ -24,6 +26,8 @@ const Lobby = () => {
       const deckToAdd = availableDecks.find((deck) => deck.id === deckID);
       if (deckToAdd) {
         setChosenDecks([...chosenDecks, deckToAdd]);
+
+        //TODO: rpc.send('room.addDeck', {deckId: deckId}, false)
         console.log(`Adding deck ${deckID} to room. Send this to server`);
       }
     }
@@ -31,26 +35,67 @@ const Lobby = () => {
 
   const removeDeck = (deckID) => {
     setChosenDecks(chosenDecks.filter((deck) => deck.id !== deckID));
+
+    //TODO: rpc.send('room.removeDeck', {deckId: deckId}, false)
     console.log(`Removing deck ${deckID} from room. Send this to server`);
   };
 
+  const fetchAvailableDecks = async () => {
+    try {
+      const decksFromServer = await rpc.send('deck.getAllDecks', {}, false);
+      const decks = decksFromServer.map((d) => ({
+        id: d.shortId,
+        title: d.title,
+        type: 'answers', // ! temp
+        author: 'Author not fetched', // ! temp
+        description: 'Description not fetched', // ! temp
+        cardCount: -1, // ! temp
+        createdAt: new Date(2020, 1, 1), // ! temp
+      }));
+      setAvailableDecks(decks);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  // Temp code to create decks on key press ('a' for answers and 'q' for questions)
+  const addDeckToServerOnKeyPress = async (e) => {
+    if (e.key === 'a' || e.key === 'q') {
+      await addGeneratedDeckToServer(userId, rpc, e.key === 'a' ? 0 : 1);
+      fetchAvailableDecks();
+    }
+  };
+
+  useEffect(async () => {
+    if (isAdmin) window.addEventListener('keydown', addDeckToServerOnKeyPress);
+    return () => window.removeEventListener(addDeckToServerOnKeyPress);
+  }, [isAdmin]);
+
+  //----------------------------------
+
   useEffect(async () => {
     try {
-      const result = await rpc.send('room.getRoom', {roomId}, false); 
+      const result = await rpc.send('room.getRoom', { roomId }, false);
       const users = result.users.map((user) => {
         return {
           isAdmin: user === result.owner,
           id: user,
           nick: user, // ! temp
           state: 'lobby', // ! temp
-          points: 0 // ! temp
-        }
-      })
+          points: 0, // ! temp
+        };
+      });
       setLeaderBoardData(users);
-    } catch (e){
+
+      if (userId === result.owner) {
+        setIsAdmin(true);
+      }
+
+      await fetchAvailableDecks();
+    } catch (e) {
       console.error(e);
     }
-  }, [])
+  }, []);
 
   return (
     <div className="lobby">
